aee8942b1397117cbf000ce2d235853ac780b101
Glen <glen@silverstripe.com>
2015-11-19 11:14:02 +1300

    changed way form handles value changes

--- src/comment-form-component.js
+++ src/comment-form-component.js
@@ -2,22 +2,51 @@ import React from 'react';
 
 class CommentFormComponent extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
     return (
         <div className="comment-form-component">
           <form className="commentForm" onSubmit={this.handleSubmit}>
-            <p><input type="text" placeholder="Name" ref="author"/></p>
-            <p><input type="email" placeholder="Email (will not be published)" ref="email"/></p>
-            <p><input type="text" placeholder="Website" ref="website"/></p>
-            <p><input type="text" placeholder="Say something..." ref="text"/></p>
-            <p><input type="submit" value="Post" /></p>
+            <p><input type="text" placeholder="Name" name="author" onChange={this.handleAuthorChange}/></p>
+            <p><input type="email" placeholder="Email (will not be published)" name="email" onChange={this.handleEmailChange}/></p>
+            <p><input type="text" placeholder="Website" name="website" onChange={this.handleWebsiteChange}/></p>
+            <p><textarea type="text" placeholder="Say something..." name="text" onChange={this.handleTextChange}/></p>
+            <p><input type="submit" value="Post"/></p>
           </form>
         </div>
     );
   }
 
-  handleSubmit(e){
+  handleAuthorChange(e) {
+     this.setState({author: e.target.value});
+  }
+
+  handleEmailChange(e) {
+     this.setState({email: e.target.value});
+  }
+
+  handleWebsiteChange(e) {
+     this.setState({website: e.target.value});
+  }
+
+  handleTextChange(e) {
+     this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
     e.preventDefault();
+
+
+    console.log("Author: " + this.state.author);
+    console.log("Email: " + this.state.password);
+    console.log("Website: " + this.state.website);
+    console.log("Text: " + this.state.text);
+    // var content = form.querySelector('[name="content"]').value;
+    // var author = this.refs.author.getDOMNode().value.trim();
     // var author = this.refs.author.value.trim();
     // var website = this.refs.website.value.trim();
     // var text = this.refs.text.value.trim();
@@ -28,6 +57,7 @@ class CommentFormComponent extends React.Component {
     // this.refs.author.value = '';
     // this.refs.website.value = '';
     // this.refs.text.value = '';
+
     console.log("CommentFormComponent -> handleSubmit()");
   }
 }
