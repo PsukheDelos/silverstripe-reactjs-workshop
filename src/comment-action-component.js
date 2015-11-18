59439f26b3c3a827120d2f4ea50103892bbc890c
Glen <glen@silverstripe.com>
2015-11-18 16:35:15 +1300

    created basic comment-action-component

---
+++ src/comment-action-component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CommentActionComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='comment-action-component'></div>
+        );
+    }
+}
+
+export default CommentActionComponent;
