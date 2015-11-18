f3e50baf8e7deb2ad8ddf0b3d8eae71d55974598
Glen <glen@silverstripe.com>
2015-11-18 15:10:55 +1300

    upgraded comment-form-component and added handleSubmit
    handleSubmit prints to the console

--- src/comment-form-component.js
+++ src/comment-form-component.js
@@ -4,18 +4,31 @@ class CommentFormComponent extends React.Component {
 
   render() {
     return (
-        <div>
-            <h3>Post your comment</h3>
-            <h4>Your name</h4>
-            <input type="text" />
-            <h4>Your email address (will not be published)</h4>
-            <input type="text" />
-            <h4>Your website URL</h4>
-            <input type="text" />
-            <h4>Comments</h4>
-            <input type="text" />
+        <div className="comment-form-component">
+          <form className="commentForm" onSubmit={this.handleSubmit}>
+            <p><input type="text" placeholder="Name" ref="author"/></p>
+            <p><input type="email" placeholder="Email (will not be published)" ref="email"/></p>
+            <p><input type="text" placeholder="Website" ref="website"/></p>
+            <p><input type="text" placeholder="Say something..." ref="text"/></p>
+            <p><input type="submit" value="Post" /></p>
+          </form>
         </div>
     );
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    // var author = this.refs.author.value.trim();
+    // var website = this.refs.website.value.trim();
+    // var text = this.refs.text.value.trim();
+    // if (!text || !author || !email) {
+    //   return;
+    // }
+    // this.props.onCommentSubmit({author: author, text: text});
+    // this.refs.author.value = '';
+    // this.refs.website.value = '';
+    // this.refs.text.value = '';
+    console.log("CommentFormComponent -> handleSubmit()");
   }
 }
 
