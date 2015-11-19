d3fa3d6c7ece7528298b4e45e5f397582c9f6bc4
[yarhtut@live.com] <yhtut@android-b1b6d7c3cfd37a1a.wgtn.silverstripe.com>
2015-11-19 14:38:10 +1300

    filtering spam and deleted comments

--- src/comment-list-component.js
+++ src/comment-list-component.js
@@ -8,16 +8,19 @@ class CommentListComponent extends React.Component {
     }
 
     render() {
+
       var commentNodes = this.props.data.map((comment, i) => {
         var props = {
           comment: comment,
           endpoints: this.props.endpoints
         };
-
-        return (
-          <CommentComponent key={i} {...props} />
-        );
+        if(comment.isSpam===0 && comment.isDelete===0){
+          return (
+            <CommentComponent key={i} {...props} />
+          );
+        }
       });
+
       return (
           <div className='comment-list-component'>
             {commentNodes}
